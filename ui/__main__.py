45d4165db3ae7a30dcdb8f0facac2b7db300d5fb
oldmud0 <oldmud0@users.noreply.github.com>
2017-09-22 12:14:49 -0500

    Create UI directory, UI entry point

---
+++ ui/__main__.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5 import QtWidgets, uic
+
+class MainWindow(QtWidgets.QMainWindow): 
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi("main.ui", self)
+        self.show()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
