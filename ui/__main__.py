7663a6f14266c3d4c70821058e1a8d91fc162727
oldmud0 <oldmud0@users.noreply.github.com>
2017-10-06 22:03:56 -0500

    Some restructuring, start laying down slots/signals
    Why did this take so long? Two reasons:
    1. College
    2. The realization of how much better PyCharm is compared to Eclipse

--- ui/__main__.py
+++ ui/__main__.py
@@ -1,16 +1,31 @@
 import sys
 from PyQt5 import QtWidgets, uic
 
+
 class MainWindow(QtWidgets.QMainWindow): 
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("main.ui", self)
         self.show()
 
+    def open_about(self):
+        QtWidgets.QMessageBox.about(self, "Animated Chatroom", "Hi")
+
+    def open_feedback(self):
+        pass
+
+    def open_guide(self):
+        pass
+
+    def open_howto(self):
+        pass
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
-    sys.exit(app.exec_()) # exec is a Python keyword, so exec_ is used
+    sys.exit(app.exec_())  # exec is a Python keyword, so exec_ is used
+
 
 if __name__ == '__main__':
     main()
